ed06335f7c84293ef273fe04325c413a43cb5077
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2023-04-03 09:02:49 +0200

    styled component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,30 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
+const StyledLink = styled(Link)`
+    padding: 15px;
+    color: #8186a0;
+    text-decoration: none;
+    font-size: 18px;
+    ${(props) =>
+        props.$isFullLink &&
+        `color: white; border-radius: 30px; background-color: #5843E4;`}
+`
+// c'est quoi le '$' ? : eh bien, cela permet de signaler à  styled-components  que notre prop nous sert pour le style, et qu'elle ne doit pas être passée dans le DOM. Ce  $  est uniquement nécessaire pour passer une prop si le composant en question est un composant React, comme ici pour  Link  (et non un élément HTML). Si mon styled component était basé sur une simple balise  a  , je pourrais totalement utiliser la prop  isFullLink  sans le  $ 
+ 
 function Header() {
-      return (
-            <nav>
-                  <Link to="/"> Accueil </Link>
-                  <Link to="/survey/42"> Questionaire</Link>
-                  <Link to="/results">résultats</Link>
-                  <Link to='/freelances'> Freelances </Link>
-            </nav>
-      );
+     return (
+          <nav>
+               <StyledLink to="/"> Accueil </StyledLink>
+               {/* $isFullLink est une props définis plud haut qui permet de styliser un de mes liens  */}
+               <StyledLink to="/survey/1" $isFullLink>
+                    {' '}
+                    Faire le test{' '}
+               </StyledLink>
+               <StyledLink to="/results"> résultats </StyledLink>
+               <StyledLink to="/freelances"> Freelances </StyledLink>
+          </nav>
+     );
 }
 
 export default Header;
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Home/index.jsx';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Survey from './pages/Survey.jsx';
 import Header from './components/Header/index.jsx';
 import Error from './components/Error/index.jsx';
@@ -10,26 +10,26 @@ import Freelances from './pages/Freelances/Freelances.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-      <React.StrictMode>
-            <Router>
-                  <Header />
-                  <Switch>
-                        <Route exact path="/">
-                              <Home />
-                        </Route>
-                        <Route path="/survey/:questionNumber">
-                              <Survey />
-                        </Route>
-                        <Route path='/results'>
-                              <Results />
-                        </Route>
-                        <Route>
-                              <Freelances />
-                        </Route>
-                        <Route>
-                              <Error />
-                        </Route>
-                  </Switch>
-            </Router>
-      </React.StrictMode>
+     <React.StrictMode>
+          <Router>
+               <Header />
+               <Switch>
+                    <Route exact path="/">
+                         <Home />
+                    </Route>
+                    <Route path="/survey/:questionNumber">
+                         <Survey />
+                    </Route>
+                    <Route path="/results">
+                         <Results />
+                    </Route>
+                    <Route path="/freelances">
+                         <Freelances />
+                    </Route>
+                    <Route>
+                         <Error />
+                    </Route>
+               </Switch>
+          </Router>
+     </React.StrictMode>
 );
